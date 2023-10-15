e75f1dbc96fada65d7213a08deb6898999dd8640
gitruili <woshilirui@hotmail.com>
2023-10-15 19:22:39 +0800

    add modal

--- src/pages/Manage/Manage.tsx
+++ src/pages/Manage/Manage.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Manage.css"
 import { Link } from "react-router-dom"
-import { Col, Row, Button, Space, List } from 'antd'
+import { Col, Row, Button, Space, List, Modal, Input } from 'antd'
 import { AntDesignOutlined } from '@ant-design/icons';
 import { Avatar, Divider } from 'antd';
 import avatar from '~/assets/avatar.svg'
@@ -16,6 +16,28 @@ import c8 from '~/assets/c8.svg'
 import { CardC } from '~/components/CardC';
 
 export default function Manage() {
+  const [visible, setVisible] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    // Handle submit button click
+    console.log('Submitted value:', inputValue);
+    // call contractor here
+    setVisible(false);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  // const handleInputChange = (e) => {
+  //   setInputValue(e.target.value);
+  // };
+
   const userCollection = [
     {
       imgSrc: c1,
@@ -123,7 +145,7 @@ export default function Manage() {
         <Col span={16}>
           <Row style={{ top: '50px' }}>
             <Col span={6} style={{ margin: '10px' }}>
-              <Button type="primary" block style={{ background: '#2A282A' }}>Issue VIP card</Button>
+              <Button type="primary" block style={{ background: '#2A282A' }} onClick={showModal}>Issue VIP card</Button>
             </Col>
             <Col span={6} style={{ margin: '10px' }}>
               <Button type="primary" block style={{ background: '#2A282A' }}>VIP view</Button>
@@ -160,74 +182,23 @@ export default function Manage() {
         <Divider />
 
       </Row>
+      <Modal
+        title="Address"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="submit" type="primary" onClick={handleOk}>
+            Submit
+          </Button>,
+        ]}
+      >
+        <Input
+          value={inputValue}
+          // onChange={handleInputChange}
+          placeholder="Input your address here"
+        />
+      </Modal>
     </div>
-    // <div>
-    //   <div style={{ margin: '50px 100px' }}>
-    //     <Row>
-    //       <Col span={2}>
-    //         <Avatar
-    //           size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-    //           src={avatar}
-    //         />
-    //       </Col>
-    //       <Col className='label' span={4}>
-    //         <p className='text-wrapper'>Daniel.eth</p>
-    //       </Col>
-    //       <Col className='label' span={18}>
-    //         <Row style={{ top: '50px' }}>
-    //           <Col span={6} style={{ margin: '10px' }}>
-    //             <Button type="primary" block style={{ background: '#2A282A' }}>Issue VIP card</Button>
-    //           </Col>
-    //           <Col span={6} style={{ margin: '10px' }}>
-    //             <Button type="primary" block style={{ background: '#2A282A' }}>VIP view</Button>
-    //           </Col>
-    //           <Col span={6} style={{ margin: '10px' }}>
-    //             <Button type="primary" block style={{ background: '#2A282A' }}>Points management</Button>
-    //           </Col>
-    //         </Row>
-    //       </Col>
-    //     </Row>
-    //     <Divider />
-    //     <Row>
-    //       <Col className='label' span={6}>
-    //         <List
-    //           itemLayout="horizontal"
-    //           dataSource={data}
-    //           renderItem={(item, index) => (
-    //             <List.Item  style={{ border: '2px solid #000', width: '80%', padding: '10px' }}>
-    //               <p>{index+1}</p>
-    //               <List.Item.Meta
-    //                 avatar={<Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />}
-    //                 title={item.title}
-    //                 // description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-    //               />
-    //             </List.Item>
-    //           )}
-    //         />
-    //       </Col>
-    //       <Col className='label' span={18}>
-    //         <Row>
-    //           {userCollection.map(item => {
-    //             return (
-    //               <Col span={6}>
-    //                 <CardC imgAlt={item.imgAlt} imgSrc={item.imgSrc} title={item.title} desc={item.desc}></CardC>
-    //               </Col>
-    //             )
-    //           })}
-    //         </Row>
-    //         <Divider />
-    //         <Row>
-    //           {userCollection2.map(item => {
-    //             return (
-    //               <Col span={6}>
-    //                 <CardC imgAlt={item.imgAlt} imgSrc={item.imgSrc} title={item.title} desc={item.desc}></CardC>
-    //               </Col>
-    //             )
-    //           })}
-    //         </Row>
-    //       </Col>
-    //     </Row>
-    //   </div>
-    // </div>
   )
 }
